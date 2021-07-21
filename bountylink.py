f6928b3570b9938144ebc873f992c5d1d9ca35c2
Andrix44 <wasd.wasd.aa@gmail.com>
2021-07-21 20:23:45 +0200

    Some more tools

--- bountylink.py
+++ bountylink.py
@@ -4,9 +4,19 @@ import coloredlogs
 import logging
 import os
 import re
+import requests
 from shutil import copyfile
 import subprocess
 import yaml
+
+def Unique(items):
+    seen = set()
+    for i in range(len(items)-1, -1, -1):
+        it = items[i]
+        if it in seen:
+            del items[i]
+        else:
+            seen.add(it)
 
 def Setup():
     global project
@@ -57,18 +67,19 @@ def Setup():
 
     logging.info(f"Running project {project['general']['name']}")
 
-def RunTool(cmd, name, timeout_s=None):
+def RunTool(cmd, name, timeout_s=None, capture=False):
     logging.info(f"Running {name}")
     logging.debug(f"Command: {cmd}")
     try:
-        subprocess.run(cmd, check=True, timeout=timeout_s)
+        result = subprocess.run(cmd, check=True, timeout=timeout_s, capture_output=capture)
     except subprocess.CalledProcessError:
         logging.critical(f"Something went wrong while running {name}. Exiting...")
         exit(-1)
     logging.info(f"{name} has finished running")
+    return result
 
 def Amass():
-    o_config = config["paths"]["amass_config_path"]
+    o_config = config["paths"]["amass_config"]
 
     options = project["amass"]
     o_active = options["active"]
@@ -86,7 +97,7 @@ def Amass():
         return
 
     cmd = []
-    cmd.append(paths["amass_path"])
+    cmd.append(paths["amass"])
     cmd.append("enum")
     if(o_active):
         cmd.append("-active")
@@ -104,17 +115,12 @@ def Amass():
         cmd.append("-timeout"); cmd.append(str(o_timeout))
     cmd.append("-d") ;cmd.append(domains)
     cmd.append("-dir"); cmd.append(directory + "amass")
-    if(o_config != "UNDEFINED"):
+    if(o_config):
         cmd.append("-config"); cmd.append(o_config)
 
     RunTool(cmd, "amass")
 
 def Shuffledns():
-    path = paths["shuffledns_path"]
-    massdns_path = paths["massdns_path"]
-    wordlist_path = paths["subdomain_wordlist_path"]
-    resolvers_path = paths["dns_resolvers_path"]
-
     options = project["shuffledns"]
     o_retries = options["retries"]
     o_verbose = options["verbose"]
@@ -133,18 +139,18 @@ def Shuffledns():
 
     for domain in domains:
         tempfile_path = f"/tmp/dnslist_{domain}"
-        with open(wordlist_path, 'r') as f:
+        with open(paths["subdomain_wordlist"], 'r') as f:
             subdomlist = [(line.strip() + f".{domain}\n") for line in f]
         with open(tempfile_path, 'w') as f:
             f.writelines(subdomlist)
 
         cmd = []
-        cmd.append(path)
+        cmd.append(paths["shuffledns"])
         cmd.append("-d"); cmd.append(domain)
         cmd.append("-list"); cmd.append(tempfile_path)
-        cmd.append("-r"); cmd.append(resolvers_path)
+        cmd.append("-r"); cmd.append(paths["dns_resolvers"])
         cmd.append("-o"); cmd.append(newdir + f"{domain}.txt")
-        cmd.append("-massdns"); cmd.append(massdns_path)
+        cmd.append("-massdns"); cmd.append(paths["massdns"])
         cmd.append("-retries"); cmd.append(str(o_retries))
         if(o_verbose):
             cmd.append("-v")
@@ -158,15 +164,6 @@ def Shuffledns():
         os.remove(tempfile_path)
 
 def SDomainCleanup():
-    def Unique(items):
-        seen = set()
-        for i in range(len(items)-1, -1, -1):
-            it = items[i]
-            if it in seen:
-                del items[i]
-            else:
-                seen.add(it)
-
     logging.info("Cleaning up subdomains...")
     blacklists = project["general"]["blacklists"]
     urls = []
@@ -218,7 +215,43 @@ def Subdomains():
     if(project["amass"]["enabled"] or project["shuffledns"]["enabled"]):
         SDomainCleanup()
 
+def Httprobe():
+    path = paths["httprobe"]
+
+    options = project["httprobe"]
+    o_threads = options["threads"]
+    o_timeout = options["timeout"]
+
+    if(not options["enabled"]):
+        logging.info("httprobe disabled. Skipping...")
+        return
+
+    logging.info(f"Running httprobe")
+
+    cmd = []
+    cmd.append("cat"); cmd.append(directory + "bl_subdomains/cleaned.txt")
+    logging.debug(f"Command part 1: {cmd}")
+    hosts = subprocess.run(cmd, check=True, capture_output=True)
+    
+    cmd = []
+    cmd.append(path);
+    cmd.append("--prefer-https")
+    cmd.append("-c"); cmd.append(str(o_threads))
+    cmd.append("-t"); cmd.append(str(o_timeout))
+    logging.debug(f"Command part 2: {cmd}")
+    found = subprocess.run(cmd, input=hosts.stdout, capture_output=True)
+
+    newdir = directory + "httprobe/"
+    os.makedirs(newdir, exist_ok=True)
+
+    with open(newdir + "webservers.txt", 'w') as f:
+        f.write(found.stdout.decode())
+
+    logging.info(f"httprobe has finished running")
+
 def Eyewitness():
+    user_agent = config["user_agent"]
+
     options = project["eyewitness"]
     o_prependhttps = options["prependhttps"]
     o_noprompt = options["noprompt"]
@@ -233,11 +266,11 @@ def Eyewitness():
         return
 
     cmd = []
-    cmd.append(paths["eyewitness_path"])
-    cmd.append("-f"); cmd.append(directory + "bl_subdomains/cleaned.txt")
+    cmd.append(paths["eyewitness"])
+    cmd.append("-f"); cmd.append(directory + "httprobe/webservers.txt")
     cmd.append("-d"); cmd.append(directory + "eyewitness")
-    if(o_prependhttps):
-        cmd.append("--prepend-https")
+    if(user_agent):
+        cmd.append("--user-agent"); cmd.append(user_agent)
     if(o_noprompt):
         cmd.append("--no-prompt")
     if(o_skipdns):
@@ -266,8 +299,8 @@ def Takeover():
     os.makedirs(newdir, exist_ok=True)
 
     cmd = []
-    cmd.append(paths["takeover_path"])
-    cmd.append("-l"); cmd.append(directory + "bl_subdomains/cleaned.txt")
+    cmd.append(paths["takeover"])
+    cmd.append("-l"); cmd.append(directory + "httprobe/webservers.txt")
     cmd.append("-o"); cmd.append(newdir + "takeover.txt")
     cmd.append("-t"); cmd.append(str(o_threads))
     cmd.append("-T"); cmd.append(str(o_timeout))
@@ -279,12 +312,16 @@ def AvailableForPurchase():
         logging.info("availableForPurchase disabled. Skipping...")
         return
 
+    logging.info(f"Running availableforpurchase")
+
     cmd = []
     cmd.append("echo"); cmd.append('\n'.join(project["general"]["rootdomains"]))
+    logging.debug(f"Command part 1: {cmd}")
     hosts = subprocess.run(cmd, check=True, capture_output=True)
     
     cmd = []
-    cmd.append("python3"); cmd.append(paths["availableforpurchase_path"])
+    cmd.append("python3"); cmd.append(paths["availableforpurchase"])
+    logging.debug(f"Command part 2: {cmd}")
     available = subprocess.run(cmd, input=hosts.stdout, capture_output=True)
 
     output = available.stdout.decode()
@@ -294,9 +331,78 @@ def AvailableForPurchase():
     with open(newdir + "output.txt", 'w') as f:
         f.write(output)
 
+    logging.info(f"availableforpurchase has finished running")
+
 def Takeovers():
     Takeover()
     AvailableForPurchase()
+
+def Webanalyze():
+    path = config["paths"]["webanalyze"]
+
+    options = project["webanalyze"]
+    o_redirect = options["redirect"]
+    o_crawl = options["crawl"]
+    o_worker = options["worker"]
+    o_silent = options["silent"]
+    o_update = options["update"]
+
+    if(not options["enabled"]):
+        logging.info("webanalyze disabled. Skipping...")
+        return
+
+    newdir = directory + "webanalyze/"
+    os.makedirs(newdir, exist_ok=True)
+
+    cmd = []
+    cmd.append(path)
+    cmd.append("-hosts"); cmd.append(directory + "httprobe/webservers.txt")
+    cmd.append("-apps"); cmd.append("/".join(path.split("/")[:-1]) + "/technologies.json") # assumes that the apps file is in the same directory as the elf
+    cmd.append("-output"); cmd.append("stdout") # the output can be later turned into json for other tools, but for now readable output is better
+    if(o_redirect):
+        cmd.append("-redirect")
+    if(o_silent):
+        cmd.append("-silent")
+    if(o_update):
+        cmd.append("-update")
+    if(o_crawl > 0):
+        cmd.append("-crawl"); cmd.append(str(o_crawl))
+    cmd.append("-worker"); cmd.append(str(o_worker))
+
+    result = RunTool(cmd, "webanalyze", capture=True)
+    with open(newdir + "webanalyze.txt", 'w') as f:
+        f.write(result.stdout.decode())
+
+def Corsy():
+    path = paths["corsy"]
+    user_agent = config["user_agent"]
+
+    options = project["corsy"]
+    o_quiet = options["quiet"]
+    o_threads = options["threads"]
+    o_delay = options["delay"]
+
+    if(not options["enabled"]):
+        logging.info("corsy disabled. Skipping...")
+        return
+
+    newdir = directory + "corsy/"
+    os.makedirs(newdir, exist_ok=True)
+
+    cmd = []
+    cmd.append("python3"); cmd.append(path)
+    cmd.append("-i"); cmd.append(directory + "httprobe/webservers.txt")
+    cmd.append("-o"); cmd.append(newdir + "corsy.json")
+    if(o_quiet):
+        cmd.append("-q")
+    if(o_threads > 0):
+        cmd.append("-t"); cmd.append(str(o_threads))
+    if(o_delay > 0):
+        cmd.append("-d"); cmd.append(str(o_delay))
+    if(user_agent):
+        cmd.append("--headers"); cmd.append("User-Agent: " + user_agent)
+
+    RunTool(cmd, "corsy")
 
 def Gau():
     o_threads = project["gau"]["threads"]
@@ -305,11 +411,12 @@ def Gau():
         logging.info("gau disabled. Skipping...")
         return
 
-    with open(directory + "bl_subdomains/cleaned.txt", 'r') as f:
+    with open(directory + "bl_subdomains/cleaned.txt", 'r') as f: # also pull stuff from subdomains that aren't running webservers anymore
         subdomains = [line.strip() for line in f]
 
-    with open(directory + "bl_subdomains/blocked.txt", 'r') as f: # out of scope subdomains shouldn't be an issue here, this just pulls stuff from the web
-        subdomains += [line.strip() for line in f]
+    if(os.path.isfile(directory + "bl_subdomains/blocked.txt")):
+        with open(directory + "bl_subdomains/blocked.txt", 'r') as f: # out of scope subdomains shouldn't be an issue here, this just pulls stuff from the web
+            subdomains += [line.strip() for line in f]
 
     newdir = directory + "gau/"
     os.makedirs(newdir, exist_ok=True)
@@ -317,7 +424,7 @@ def Gau():
     for subdom in subdomains:
         filepath = newdir + f"{subdom}.txt"
         cmd = []
-        cmd.append(paths["gau_path"])
+        cmd.append(paths["gau"])
         cmd.append("-o"); cmd.append(filepath)
         cmd.append("-t"); cmd.append(str(o_threads))
         cmd.append(subdom)
@@ -329,21 +436,31 @@ def Gau():
             logging.error(f"gau({subdom}) has timed out and was killed.")
 
 def Gitleaks():
-    o_verbose = project["gitleaks"]["verbose"]
-    o_quiet = project["gitleaks"]["quiet"]
+    options = project["gitleaks"]
+    o_skip_forks = options["skip_forks"]
+    o_verbose = options["verbose"]
+    o_quiet = options["quiet"]
 
     repos = project["general"]["git_repos"]
+    orgs = project["general"]["git_orgs"]
 
-    if(not project["gitleaks"]["enabled"] or not repos):
-        logging.info("availableForPurchase disabled. Skipping...")
+    if(not project["gitleaks"]["enabled"] or (not repos and not orgs)):
+        logging.info("gitleaks disabled or no inputs provided. Skipping...")
         return
 
     newdir = directory + "gitleaks/"
     os.makedirs(newdir, exist_ok=True)
 
+    for org in orgs:
+        r = requests.get(f"https://api.github.com/orgs/{org}/repos")
+        data = yaml.safe_load(r.text)
+        for obj in data:
+            if(not obj["fork"] or not o_skip_forks):
+                repos.append(obj["html_url"])
+
     for repo in repos:
         cmd = []
-        cmd.append(paths["gitleaks_path"])
+        cmd.append(paths["gitleaks"])
         cmd.append("-r"); cmd.append(repo)
         cmd.append("-o"); cmd.append(newdir +  f"{'_'.join(repo.split('/')[-2:])}.json")
         cmd.append("--leaks-exit-code"); cmd.append("0")
@@ -353,11 +470,20 @@ def Gitleaks():
             cmd.append("-q")
         RunTool(cmd, f"gitleaks({repo})")
 
+    leaks = os.listdir(newdir)
+    for leak in leaks:
+        if(os.path.getsize(newdir + leak) == 5): # remove the files If no leaks were found
+            os.remove(newdir + leak)
+    logging.info("Cleaned up the results that had no leaks")
+
 if(__name__ == "__main__"):
     print("Bountylink by Andrix")
     Setup()
     Subdomains()
+    Httprobe()
     Eyewitness()
     Takeovers()
+    Webanalyze()
+    Corsy()
     Gau()
     Gitleaks()
